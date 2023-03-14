77b3f03f5ae9cbd903f9bc7af36f093f5b8590dc
Steven <stevenlgtm@gmail.com>
2023-03-15 07:47:00 +0800

    chore: upgrade vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8082/",
-        changeOrigin: true,
-      },
-      "/o": {
         target: "http://localhost:8082/",
         changeOrigin: true,
       },
