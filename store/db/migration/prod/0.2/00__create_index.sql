4279151238d1edca2da9033d3fffb9015a8801e6
Steven <stevenlgtm@gmail.com>
2023-07-14 20:42:10 +0800

    chore: upgrade version to `v0.2.0`

--- store/db/migration/prod/0.2/00__create_index.sql
+++ store/db/migration/prod/0.2/00__create_index.sql
@@ -1,2 +1,2 @@
-CREATE INDEX idx_user_email ON user(email);
-CREATE INDEX idx_shortcut_name ON shortcut(name);
+CREATE INDEX IF NOT EXISTS idx_user_email ON user(email);
+CREATE INDEX IF NOT EXISTS idx_shortcut_name ON shortcut(name);
