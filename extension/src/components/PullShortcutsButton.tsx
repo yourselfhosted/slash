11e062549a13b2fc7afd06e6479db8a5793b254d
Steven <stevenlgtm@gmail.com>
2023-08-08 23:00:05 +0800

    fix: list shortcut api url

--- extension/src/components/PullShortcutsButton.tsx
+++ extension/src/components/PullShortcutsButton.tsx
@@ -24,7 +24,7 @@ const PullShortcutsButton = () => {
       setIsPulling(true);
       const {
         data: { shortcuts },
-      } = await axios.get<ListShortcutsResponse>(`${domain}/api/v2/shortcut`, {
+      } = await axios.get<ListShortcutsResponse>(`${domain}/api/v2/shortcuts`, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
