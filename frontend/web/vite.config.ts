6ee6a5166efb9f7f4da7a4481e6f874e712d9fe7
Steven <stevenlgtm@gmail.com>
2023-09-30 01:23:25 +0800

    chore: update dev server config

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -2,6 +2,12 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
+let devProxyServer = "http://localhost:8082/";
+if (process.env.DEV_PROXY_SERVER && process.env.DEV_PROXY_SERVER.length > 0) {
+  console.log("Use devProxyServer from environment: ", process.env.DEV_PROXY_SERVER);
+  devProxyServer = process.env.DEV_PROXY_SERVER;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,13 +15,17 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
     proxy: {
-      "/api/": {
-        target: "http://localhost:8082/",
-        changeOrigin: true,
+      "^/api": {
+        target: devProxyServer,
+        xfwd: true,
       },
-      "/s/": {
-        target: "http://localhost:8082/",
-        changeOrigin: true,
+      "^/slash.api.v2": {
+        target: devProxyServer,
+        xfwd: true,
+      },
+      "^/s/": {
+        target: devProxyServer,
+        xfwd: true,
       },
     },
   },
