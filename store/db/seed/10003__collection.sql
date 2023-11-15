3da0e4720e9c69b7d8f382213cb16e385ca2e1c1
Steven <stevenlgtm@gmail.com>
2023-11-16 07:38:09 +0800

    chore: add collection seed data

---
+++ store/db/seed/10003__collection.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+  collection (
+    `id`,
+    `creator_id`,
+    `name`,
+    `title`,
+    `description`,
+    `visibility`,
+    `shortcut_ids`
+  )
+VALUES
+  (
+    1,
+    101,
+    'minecraft',
+    'Minecraft',
+    '',
+    'PUBLIC',
+    '1,2,3,4,5'
+  );
