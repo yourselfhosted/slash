91708da5fcaf108c6a29e4a15a21dfc9f7c6d50e
Steven <stevenlgtm@gmail.com>
2023-12-24 00:32:29 +0800

    chore: update dev version

--- server/version/version.go
+++ server/version/version.go
@@ -12,7 +12,7 @@ import (
 var Version = "0.5.0"
 
 // DevVersion is the service current development version.
-var DevVersion = "0.5.0"
+var DevVersion = "0.5.1"
 
 func GetCurrentVersion(mode string) string {
 	if mode == "dev" || mode == "demo" {
