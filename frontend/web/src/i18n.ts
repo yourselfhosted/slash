61d01a53eb7f8331f630cf79dbadc7818b446a08
Steven <stevenlgtm@gmail.com>
2024-07-29 08:45:26 +0800

    chore: tweak linter warning

--- frontend/web/src/i18n.ts
+++ frontend/web/src/i18n.ts
@@ -2,11 +2,11 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "../../locales/en.json";
 import fr from "../../locales/fr.json";
+import hu from "../../locales/hu.json";
 import ja from "../../locales/ja.json";
 import ru from "../../locales/ru.json";
 import tr from "../../locales/tr.json";
 import zh from "../../locales/zh.json";
-import hu from "../../locales/hu.json";
 
 i18n.use(initReactI18next).init({
   resources: {
