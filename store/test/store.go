feb952f7ac553a57c6aa009cedb1f8f1b1bc5209
johnnyjoy <yourselfhosted@gmail.com>
2025-05-07 21:12:22 +0800

    chore: update store tests

--- store/test/store.go
+++ store/test/store.go
@@ -3,16 +3,20 @@ package teststore
 import (
 	"context"
 	"fmt"
+	"net"
+	"os"
 	"testing"
 
+	"github.com/joho/godotenv"
+
+	"github.com/yourselfhosted/slash/server/common"
 	"github.com/yourselfhosted/slash/server/profile"
 	"github.com/yourselfhosted/slash/store"
 	"github.com/yourselfhosted/slash/store/db"
-	"github.com/yourselfhosted/slash/test"
 )
 
 func NewTestingStore(ctx context.Context, t *testing.T) *store.Store {
-	profile := test.GetTestingProfile(t)
+	profile := getTestingProfile(t)
 	dbDriver, err := db.NewDBDriver(profile)
 	if err != nil {
 		fmt.Printf("failed to create db driver, error: %+v\n", err)
@@ -40,4 +44,49 @@ func resetTestingDB(ctx context.Context, profile *profile.Profile, dbDriver stor
 			panic(err)
 		}
 	}
+}
+
+func getUnusedPort() int {
+	// Get a random unused port
+	listener, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	// Get the port number
+	port := listener.Addr().(*net.TCPAddr).Port
+	return port
+}
+
+func getTestingProfile(t *testing.T) *profile.Profile {
+	if err := godotenv.Load(".env"); err != nil {
+		t.Log("failed to load .env file, but it's ok")
+	}
+
+	// Get a temporary directory for the test data.
+	dir := t.TempDir()
+	mode := "prod"
+	port := getUnusedPort()
+	driver := getDriverFromEnv()
+	dsn := os.Getenv("DSN")
+	if driver == "sqlite" {
+		dsn = fmt.Sprintf("%s/slash_%s.db", dir, mode)
+	}
+	return &profile.Profile{
+		Mode:    mode,
+		Port:    port,
+		Data:    dir,
+		DSN:     dsn,
+		Driver:  driver,
+		Version: common.GetCurrentVersion(mode),
+	}
+}
+
+func getDriverFromEnv() string {
+	driver := os.Getenv("DRIVER")
+	if driver == "" {
+		driver = "sqlite"
+	}
+	return driver
 }
